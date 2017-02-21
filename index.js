4e444a934e9720b7ef8181fda1bfb3ea7937a945
Joseph Wunz <jodaman19@gmail.com>
2017-02-21 15:24:53 -0700

    Got the information about burgers to display, just the image to go.

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.use(express.static(path.join(__dirname + '/public')));
 app.get('/', function(req, res){
     res.render('index');
 });
-app.get('/:viewname', function(req, res){
-    res.render(req.params.viewname);
-});
 app.get('/menu1', function(req, res){
     res.render('menu1', data);
+});
+app.get('/:viewname', function(req, res){
+    res.render(req.params.viewname);
 });
 
 app.listen(3000);
